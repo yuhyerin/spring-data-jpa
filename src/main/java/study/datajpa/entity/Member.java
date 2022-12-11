7e6dfdae2fd479d655187cb00cdccec2167dde38
RiRi <hyerin77@naver.com>
2022-12-11 22:41:58 +0900

    예제 도메인 모델, 동작 확인

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,29 +1,46 @@
 package study.datajpa.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id","username","age"})
 public class Member {
     @Id
     @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
 
-    // entity 는 기본생성자 반드시 있어야 한다.
-    protected Member(){
-
-    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name= "team_id")
+    private Team team;
+    // entity 는 기본생성자 반드시 있어야 한다. -> lombok 어노테이션으로 대체
+//    protected Member(){
+//
+//    }
     public Member(String username){
         this.username=username;
     }
 
+    public Member(String username, int age, Team team) {
+        this.username =username;
+        this.age = age;
+        if(team!=null){
+            changeTeam(team);
+        }
+    }
+
     public void changeUsername(String username){
         this.username = username;
+    }
+
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
